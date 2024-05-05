dff28b17747682ea01b52831dff9613df2500e29
devops-prince <dev.prince116@gmailcom>
2024-05-06 04:55:29 +0530

    updated

--- primeNumber.cpp
+++ primeNumber.cpp
@@ -1,49 +1,56 @@
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// int isPrime(int n) {   // you can also make it a boolean function;
+int isPrime(int n)
+{ // you can also make it a boolean function;
 
-//   // check if n is 1 or 0 , if yes they are not prime so return false;
+    // check if n is 1 or 0 , if yes they are not prime so return false;
 
-//   if (n == 1 || n == 0) {
-//     return false;
-//   };
+    if (n == 1 || n == 0)
+    {
+        return false;
+    };
 
-//   // iterate a loop from 2 ;
+    // iterate a loop from 2 ;
 
-//   for (int i = 2; i < n; i++) {
+    for (int i = 2; i < n; i++)
+    {
 
-//     // check if n is divisible by i or not;
+        // check if n is divisible by i or not;
 
-//     if (n % i == 0) { // if divisible then return false;
-//       return false;
-//     };
-//   };
+        if (n % i == 0)
+        { // if divisible then return false;
+            return false;
+        };
+    };
 
-//   return true;
-// }
+    return true;
+}
 
-// int main() {
+int main()
+{
 
-//   // print prime numbers from 1 to 100 ;
+    // print prime numbers from 1 to 100 ;
 
-//   // for (int n = 1; n <= 100; n++) {
+    // for (int n = 1; n <= 100; n++) {
 
-//   //   if (isPrime(n)) {
-//   //     cout << n << " ";
-//   //   };
-//   // };
+    //   if (isPrime(n)) {
+    //     cout << n << " ";
+    //   };
+    // };
 
-//   // using while loop ;
+    // using while loop ;
 
-//   int n = 1;
+    int n = 1;
 
-//   while (n <= 100) {
-//     if (isPrime(n)) {
-//       cout << n << " ";
-//     }
-//     n++;
-//   }
+    while (n <= 100)
+    {
+        if (isPrime(n))
+        {
+            cout << n << " ";
+        }
+        n++;
+    }
 
-//   return 0;
-// };
+    return 0;
+};
--- sip_calculator.cpp
+++ sip_calculator.cpp
@@ -3,53 +3,51 @@
 
 using namespace std;
 
-
-double calculateSIP(double amount , double annualInterest , int timePeriod){
+double calculateSIP(double amount, double annualInterest, int timePeriod)
+{
 
   // number of installment paid in for-ex: 10yrs so 10*12 = 120
   int installment = timePeriod * 12;
 
-  // monthly interest on principle amount 
-  double monthlyInterest = annualInterest/12/100; 
+  // monthly interest on principle amount
+  double monthlyInterest = annualInterest / 12 / 100;
 
   // total amount returned after investment
   double matuarityAmount = 0;
-  
-  // now create a loop that will calculate maturity amount for each months compounding
-  for(int i = 0; i < installment; i++){
 
-    // applyin formula for  adding all amount with monthly interest 
-     //(compounding )
-    matuarityAmount=(matuarityAmount + amount)*(1 + monthlyInterest);
+  // now create a loop that will calculate maturity amount for each months compounding
+  for (int i = 0; i < installment; i++)
+  {
+
+    // applyin formula for  adding all amount with monthly interest
+    //(compounding )
+    matuarityAmount = (matuarityAmount + amount) * (1 + monthlyInterest);
   };
 
   return matuarityAmount;
-
 };
 
 int main()
 {
-  double amount ,annualInterest;
+  double amount, annualInterest;
   int timePeriod;
 
   // ****************** Take inputs
 
   // principle amount
-  cout<< "Enter Amount you want to invest each month: "<<endl;
-  cin>>amount;
+  cout << "Enter Amount you want to invest each month: " << endl;
+  cin >> amount;
 
   // Rate of Interest
-  cout<< "Enter rate of interest you are getting in (%): "<<endl;
-  cin>>annualInterest;
+  cout << "Enter rate of interest you are getting in (%): " << endl;
+  cin >> annualInterest;
 
   // Time Period for which you want to invest
-  cout<< "Enter Time period for you want to invest: "<<endl;
-  cin>>timePeriod;
+  cout << "Enter Time period for you want to invest: " << endl;
+  cin >> timePeriod;
 
   // call calculateSip
-  int result = calculateSIP(amount,annualInterest,timePeriod);
-  cout<< "Total matuarity amount: "<<result  <<endl;
+  int result = calculateSIP(amount, annualInterest, timePeriod);
+  cout << "Total matuarity amount: " << result << endl;
   return 0;
-  
-  
 }
